b2be71fc07d168c1e2081c9272dd1efad0e094a0
kevin flores <kevindannerflores@gmail.com>
2025-10-27 21:18:21 -0400

    feat(api): crear controlador y rutas GET/POST empleados

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
 <?php
 
+use App\Http\Controllers\EmpleadoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
+Route::apiResource('empleados', EmpleadoController::class);
